031c456cc10bd060c9ff2109810e10a44d091971
Predka <swaggajugga69@gmail.com>
2022-12-24 21:33:22 +0700

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+//  которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int CheckArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (arr[i] % 2 == 0) {
+        count++;
+       } 
+    }
+    return count;
+}
+
+int[] CrateArray(int size, int begin, int end )
+{
+int[] arr = new int[size];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(begin, end + 1);
+}
+return arr;
+}
+
+int[] arr = CrateArray(10, 100, 999);
+int count = CheckArray(arr);
+Console.WriteLine($"[{(String.Join(" ", arr))}] -> {count}");
